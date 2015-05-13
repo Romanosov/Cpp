7e1fd612bdbe24ccc8be2422467bf2f518c3f717
Romanosov <Romansamokhvalov@mail.ru>
2015-05-13 21:34:29 +0300

    Hometask #3

---
+++ SettingsLib/settings.cpp
@@ -0,0 +1,220 @@
+#include "settings.h"
+
+settings::settings(std::string const & filename)   {
+    this->filename = filename;
+    this->reload();
+}
+
+void settings::reload() {
+    char *chr = new char[this->filename.length() + 1];
+    strcpy(chr, this->filename.c_str());
+    this->stored.clear();
+    string str0, str1;
+    std::ifstream input(chr);
+    while (getline(input, str0)) {
+        getline(input, str1);
+        this->stored[str0] = str1;
+    };
+    input.close();
+};
+
+string const & settings::get(std::string const & name, std::string const & def) const {
+    if(stored.find(name) != stored.end())  {
+        return stored.find(name)->second;
+    }
+    else  {
+        return def;
+    };
+}
+
+void settings::set(std::string const & name, std::string const & value)  {
+    this->stored[name] = value;
+    char* chr = new char[filename.length() + 1];
+    strcpy(chr, filename.c_str());
+    std::ofstream output(chr);
+    map <string, string> ::iterator i;
+    for (i = stored.begin(); i != stored.end(); i++) {
+        output << (*i).first << endl;
+        output << (*i).second << endl;
+    }
+    output.close();
+}
+
+void settings::reset() {
+    char *chr = new char[filename.length() + 1];
+    strcpy(chr, filename.c_str());
+    ofstream(chr, std::ofstream::trunc);
+    stored.clear();
+}
+
+settings::param::param(const string & name, std::string value, const settings *parent) {
+    this->value = value;
+    this->name = name;
+    this->parent = const_cast<settings* const>(parent);
+}
+
+settings::param::operator std::string() const {
+    return string(value);
+}
+
+settings::param::operator bool() const {
+    return (value == "true");
+}
+
+settings::param::operator int() const {
+    return atoi(value.c_str());
+}
+
+settings::param::operator double() const {
+    double z = ::atof(value.c_str());
+    return z;
+}
+
+settings::param & settings::param::operator = (char const * value) {
+    return this->operator=(std::string(value));
+}
+
+settings::param & settings::param::operator = (std::string const & value) {
+    this->value = value;
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator = (int value) {
+    stringstream stst;
+    stst << value;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator = (bool value) {
+    ostringstream stst;
+    stst << boolalpha << value;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator = (double value) {
+    ostringstream stst;
+    stst << value;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator += (std::string const & value) {
+    this->value += value;
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator += (int value) {
+    stringstream stst;
+    int dem = atoi(this->value.c_str()) + value;
+    stst << dem;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator += (double value) {
+    double zz = ::atof(this->value.c_str());
+    zz = zz + value;
+    ostringstream stst;
+    stst << zz;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator -= (int value) {
+    stringstream stst;
+    int dem = atoi(this->value.c_str()) - value;
+    stst << dem;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator -= (double value) {
+    double zz = ::atof(this->value.c_str());
+    zz = zz - value;
+    ostringstream stst;
+    stst << zz;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator *= (int value) {
+    stringstream stst;
+    int dem = atoi(this->value.c_str()) * value;
+    stst << dem;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator *= (double value) {
+    double zz = ::atof(this->value.c_str());
+    zz = zz * value;
+    ostringstream stst;
+    stst << zz;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator /= (int value) {
+    stringstream stst;
+    int dem = atoi(this->value.c_str()) / value;
+    stst << dem;
+    this->value = ss.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator /= (double value) {
+    double zz = ::atof(this->value.c_str());
+    zz = zz / value;
+    ostringstream stst;
+    stst << zz;
+    this->value = stst.str();
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator|=(bool value) {
+    if(value){
+        this->value="true";
+    };
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+settings::param & settings::param::operator&=(bool value) {
+    if(!value){
+        this->value="false";
+    };
+    parent->set(this->name, this->value);
+    return *this;
+}
+
+bool settings::param::is_empty() const {
+    return this->value.empty();
+}
+
+const settings::param settings::operator[](std::string const &name) const {
+    return param(name, stored.find(name)->second, this);
+}
+
+settings::param settings::operator[](std::string const & name) {
+    return param(name, stored.find(name)->second, this);
+}
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
---
+++ SettingsLib/settings.h
@@ -0,0 +1,115 @@
+#ifndef SETTINGS_H_INCLUDED
+#define SETTINGS_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+class settings {
+public:
+
+
+    class param {
+        friend class settings;
+    private:
+        param(const string &, const string , const settings *);
+        string name, value;
+        settings *parent;
+    public:
+        operator std::string() const;
+        operator int() const;
+        operator bool() const;
+        operator double() const;
+
+        param & operator=(char const *value);
+        param & operator=(std::string const &);
+        param & operator=(int);
+        param & operator=(bool);
+        param & operator=(double);
+
+        param & operator+=(std::string const &);
+        param & operator+=(int);
+        param & operator+=(double);
+
+        param & operator-=(int);
+        param & operator-=(double);
+
+        param & operator*=(int);
+        param & operator*=(double);
+
+        param & operator/=(int);
+        param & operator/=(double);
+
+        param & operator|=(bool);
+        param & operator&=(bool);
+
+        bool is_empty() const;
+    };
+    friend class param;
+
+
+
+/**
+     * Construct settings store
+     * and load data from file (if exists)
+     * \param filename Path to file with settings
+     */
+
+    settings(std::string const & filename);
+
+/**
+     * Get setting value
+     * \param name Setting unique identifier
+     * \param def Default setting value
+     * \return Stored value for given name or default value
+     */
+
+    std::string const & get(std::string const & name, std::string const & def = "") const;
+
+/**
+     * Set or replace setting value and save changes to file
+     * \param name Setting unique identifier
+     * \param value New setting value
+     */
+
+    void set(std::string const & name, std::string const & value);
+
+/**
+     * Reset all settings
+     */
+
+    void reset();
+
+/**
+     * Reload all settings from file
+     */
+
+    void reload();
+
+
+
+
+/**
+      * Get constant setting wrapper
+      * \param name Setting unique identifier
+      */
+
+    const param operator[](std::string const & name) const;
+
+/**
+      * Get constant setting wrapper
+      * \param name Setting unique identifier
+      */
+
+    param operator[](std::string const & name);
+private:
+string filename;
+    map<string, string> stored;
+
+};
+
+#endif // SETTINGS_H_INCLUDED
